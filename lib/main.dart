62bd68277c5afb34741464ae9604eef2f914264d
Adhil shah <adhilshaht111@gmail.com>
2026-02-07 13:27:05 +0530

    initial setUp

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:locami/bindings/main_bindings.dart';
+import 'package:locami/navigation/main_nav.dart';
+import 'package:locami/theme/app_theme.dart';
+import 'package:locami/theme/them_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MainApp());
+  WidgetsFlutterBinding.ensureInitialized();
+
+  ThemeProvider.instance.setMatchWithSystem(true);
+
+  runApp(
+    ChangeNotifierProvider.value(
+      value: ThemeProvider.instance,
+      child: const MainApp(),
+    ),
+  );
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    final themeProvider = context.watch<ThemeProvider>();
+
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialBinding: MainBindings(),
+      theme: materialLightTheme,
+      darkTheme: materialDarkTheme,
+
+      themeMode:
+          themeProvider.isMatchWithSystem
+              ? ThemeMode.system
+              : (themeProvider.theme == AppThemeMode.dark
+                  ? ThemeMode.dark
+                  : ThemeMode.light),
+
+      home: const MainNav(),
     );
   }
 }
---
+++ lib/navigation/main_nav.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MainNav extends StatefulWidget {
+  const MainNav({Key? key}) : super(key: key);
+
+  @override
+  _MainNavState createState() => _MainNavState();
+}
+
+class _MainNavState extends State<MainNav> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
